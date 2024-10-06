53ad61104cf5f5076c59ad73ff07215a86091bf9
Kevin Manka <113623001+kevin-manka@users.noreply.github.com>
2024-10-06 15:39:04 -0400

    feat: irregular ellipse-based dent shapes using Perlin noise

--- README.md
+++ README.md
@@ -9,8 +9,8 @@
 (1a)&nbsp;&nbsp;&nbsp;&nbsp;Sparse small hail dents\
 (1b)&nbsp;&nbsp;&nbsp;&nbsp;Sparse large hail dents\
 (2)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Monodispersed small dents with high density\
-(3)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentially distributed dents\
-(4)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentially distributed dents with high density
+(3)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentially-distributed dents\
+(4)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentially-distributed dents with high density
 
 #### Configuration
 
--- training-model/generator/generator.py
+++ training-model/generator/generator.py
@@ -1,6 +1,7 @@
 import json
 import cv2
 import numpy as np
+import noise
 
 
 def generate(diameter_range: [float, float],
@@ -12,11 +13,15 @@ def generate(diameter_range: [float, float],
     """Generate simulated hailpad depth map binarizations based on config data"""
 
     IMAGE_SIZE = 1000
+    SCALE = 0.3
 
     for i in range(hailpad_count):
         image = np.zeros((IMAGE_SIZE, IMAGE_SIZE, 3), dtype=np.uint8)
 
+        wind_angle = np.random.rand() * 360
+
         if exp:
+            # TODO: Less min-sized dents than small dents
             beta = -(diameter_range[1])/np.log(1 - 0.999)
             diameter_dist = np.random.exponential(scale=beta, size=dent_count)
             diameter_dist = diameter_dist[diameter_dist >= diameter_range[0]]
@@ -30,23 +35,52 @@ def generate(diameter_range: [float, float],
 
             # TODO: Wind-driven hail considerations:
             #   - Determine axis ratio to classify as wind-driven dent
-            #   - Determine if wind-driven dents should be of similar angles
             major_axis = diameter_dist[j]
             minor_axis = major_axis * (1 - axis_variation * np.random.rand())
-            angle = np.random.rand() * 360
 
-            cv2.ellipse(
-                image,
-                (x, y),
-                (int(major_axis / 2), int(minor_axis / 2)),
-                angle,
-                0,
-                360,
-                (255, 255, 255),
-                -1
-            )
+            if (minor_axis / major_axis) <= 0.5:
+                angle = wind_angle
+                color = (0, 255, 255)
+            else:
+                angle = np.random.rand() * 360
+                color = (255, 255, 255)
+
+            dent = create_dent(
+                x, y, major_axis, minor_axis, angle, SCALE)
+
+            cv2.drawContours(image, [dent], -1, color, -1)
 
         cv2.imwrite(f"{directory}/hailpad_{i}.png", image)
+
+
+def create_dent(cx, cy, major_axis, minor_axis, angle, scale):
+    """Helper function to create irregular dent shape from base ellipse using Perlin noise along ellipse segments"""
+
+    NUM_POINTS = 50
+    points = []
+
+    for i in range(NUM_POINTS):
+        theta = (2 * np.pi / NUM_POINTS) * i
+        x = (major_axis / 2) * np.cos(theta)
+        y = (minor_axis / 2) * np.sin(theta)
+
+        x_rot = x * np.cos(np.radians(angle)) - y * np.sin(np.radians(angle))
+        y_rot = x * np.sin(np.radians(angle)) + y * np.cos(np.radians(angle))
+
+        noise_value = noise.pnoise2(x_rot * scale,
+                                    y_rot * scale,
+                                    octaves=6,
+                                    lacunarity=0.5,
+                                    persistence=1)
+        x_noise = x_rot * (1 + noise_value)
+        y_noise = y_rot * (1 + noise_value)
+
+        x_final = int(cx + x_noise)
+        y_final = int(cy + y_noise)
+
+        points.append([x_final, y_final])
+
+    return np.array(points, dtype=np.int32)
 
 
 with open('training-model/generator/config.json', 'r') as f:
